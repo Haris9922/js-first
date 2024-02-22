731cd11c3c3ffd5ad8151b4f233453d252070e7f
Haris9922 <118390305+Haris9922@users.noreply.github.com>
2024-02-22 19:35:26 +0000

    object basic

--- new-js/objects.js
+++ new-js/objects.js
@@ -11,7 +11,8 @@ const mySym = Symbol("key1")
     name :"Harry",
     "full name" : "Haris",
     age : 24,
-    [mySym] : "mykey1",
+    [mySym] : "mykey1", 
+   //  if we use symbol then we use this syntax
     email : "haris99@gmail.com",
     location : "Isl" ,
     isloggedIn : false,
@@ -19,8 +20,23 @@ const mySym = Symbol("key1")
  }
  console.log(jsUser.email)
  console.log(jsUser["email"])
- console.log([jsUser[ "full name"] )
- console.log([jsUser[ "full name"] )
+ console.log(jsUser["full name"])
+ console.log(jsUser[mySym])
 
+// if change any value then use"="
 
- 
+ jsUser.email = "abc2gmail.com"
+ console.log(jsUser)
+ // if freeze  any value then use first call object then value
+//  Object.freeze(jsUser)
+
+// adding function in objects 
+jsUser.greeting = function(){
+   console.log("Hello everyone");
+}
+console.log(jsUser.greeting());
+// Adding any items for calling this method 
+jsUser.greetingTwo = function(){
+   console.log(`hello ,${this.name}`);
+}
+console.log(jsUser.greetingTwo());
