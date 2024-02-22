013eae42fd03ff54107aaf50f5f22b58f7f5092c
Haris9922 <118390305+Haris9922@users.noreply.github.com>
2024-02-22 20:47:21 +0000

    singleton and object literals

---
+++ new-js/singleton-or-object-literals.js
@@ -0,0 +1,30 @@
+// single object may be 
+// const tinderUser = new Object() 
+const tinderUser = {}
+tinderUser.id = "a@gmail/com"
+tinderUser.name = "Heoo"
+
+// adding object inside object 
+const jsUer1 = {
+    email: "some@gamil.com",
+    fullname:{
+        userfullname:{
+            full:"har",
+            last:"ris"
+        }
+    }
+}  
+// console.log(jsUer1.fullname);
+
+// mergin objects 
+const objt_1 = {1 : "A",2 : "B" }
+const objt_2 = {3 : "c",4 : "d" }
+const objt_3 = {5 : "e",6 : "f" }
+
+console.log( objt_1 , objt_2 , objt_3) 
+// if we merg then this methid can be use 
+// const obj_res = Object.assign({} , objt_1 , objt_2 , objt_3)
+// if we merg all then we use spread(...) 
+const obj_res = {...objt_1 , ...objt_2 , ...objt_3}
+
+console.log(obj_res)
