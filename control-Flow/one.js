2bf828ade2c150b20591fd95b6846ff49b9ace97
Haris9922 <118390305+Haris9922@users.noreply.github.com>
2024-03-13 18:34:17 +0000

    or and

--- control-Flow/one.js
+++ control-Flow/one.js
@@ -3,3 +3,26 @@ if (2 == 2){
 console.log("True");
 }
 // < lessthen, > greaterthen lessthan or equal to, <= less than or equal to, ==equal to, not equal to ,=== check the type.
+const score = 200
+if (score > 100){
+    const power="fly"
+    comsole.log(`user power:${power}`);
+}
+
+// short hand notation impleset scope
+// const balance = 1000
+// if(balance> 500) console.log("test") 
+
+// nasted statment 
+const balance = 2000
+if (balance<500){
+    console.log("less than 500");
+}else if (balance < 750){
+    console.log("less yhan 750")
+}else
+{
+    console.log("higher")
+} 
+
+
+&& And(both true) || (At least one true) 
